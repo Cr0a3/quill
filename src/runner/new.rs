c128780a9609912221f52dde6676e2a50ce4b02f
Toni Graphics <buch.toni@outlook.com>
2024-03-08 13:45:53 +0100

    templates semi working

--- src/runner/new.rs
+++ src/runner/new.rs
@@ -1,5 +1,5 @@
-use crate::print;
-use std::{env, fs, path::Path};
+use crate::{print, conf::{self, Data, Package}};
+use std::{io, env, fs, path::Path};
 use PrintLib::colorize::Colorize;
 
 pub fn new(name: &str, libary: bool, template: &str) -> std::io::Result<()>{
@@ -30,37 +30,41 @@ pub fn new(name: &str, libary: bool, template: &str) -> std::io::Result<()>{
 
     // extracting zip
     for i in 0..zip.len() {
-        let file = zip.by_index(i)?;
-        println!("Filename: {}", file.name());
+        let mut file = zip.by_index(i)?;
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        if (*file.name()).ends_with('/') {
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
     }
 
-    /*// writing files
-    let data = Data {
-        package: Package {
-            name: name.into(),
-            version: "".into(),
-            author: "".into(),
-            description: "".into(),
-        }
-    };
+    // rewrite config
+    let cfg_path = Path::new( &format!("{}/cpack.toml", template) );
+    
 
-    let toml = match toml::to_string(
-        &data ) {
-        Ok(d) => { d },
-        Err(e) => {
-            print::error("E", &format!("couldn't serialize example data: {}", e.to_string()) );
-            return;
-        }
-    };
-
-    match conf.write(toml.as_bytes()) {
-        Ok(_) => {},
-
-        Err(e) => {
-            print::error("E", &format!("error while writing cpack.toml: {}", e.to_string()));
-            return;
-        }
-    }*/
+    // rename dir to project name
+    fs::rename(template, name)?;
 
     println!("  - {} {}: '{name}'", "Created".color(0, 42, 71).bold(), match libary { true => "libary", false => "package" } );
 
