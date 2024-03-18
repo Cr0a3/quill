d45867f0dc249ec4173613b96fea045faa3c5a43
Cr0a3 <127748753+Toni-Graphics@users.noreply.github.com>
2024-03-18 13:31:41 +0000

    update

--- backend/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Currently the api gets implemented";
-
-?>
--- src/print.rs
+++ src/print.rs
@@ -2,25 +2,20 @@ use std::process::exit;
 
 use PrintLib::error::ErrorFactory;
 use PrintLib::colorize::Colorize;
+use PrintLib::arg::Arg;
 
 pub fn help() {
-    println!("{}", "C's package manager by Cr0a3".color(0, 42, 71).bold());
-    println!();
-    println!("{} {}", "Usage:".underline(), "quill [OPTIONS] [CMD]".color(0, 42, 71).bold());
-    println!();
-    println!("{}", "Options:");
-    println!("   {}\t {} {}",       "-v, --version".color(59, 4, 105),      "|".color(227, 173, 25),    "Prints version info"                           );
-    println!("   {}\t\t {} {}",     "--lib".color(59, 4, 105),              "|".color(227, 173, 25),    "Makes the new package a libary"                );
-    println!("   {}\t {} {}",       "--template (name)".color(59, 4, 105),  "|".color(227, 173, 25),    "Uses template (name)"                          );
-    println!();
-    println!("{}", "Common commands:");
-    println!("   {}\t {} {}",       "build (target)".color(59, 4, 105),     "|".color(227, 173, 25),    "Builds current package with target (target)"   );
-    println!("   {}\t\t {} {}",     "clean".color(59, 4, 105),              "|".color(227, 173, 25),    "Cleans builds"                                 );
-    println!("   {}\t\t {} {}",     "new (name)".color(59, 4, 105),         "|".color(227, 173, 25),    "Creates new package with name (name)"          );
-    println!("   {}\t\t\t {} {}",   "add".color(59, 4, 105),                "|".color(227, 173, 25),    "Adds dependenci"                               );
-    println!("   {}\t\t {} {}",     "publish".color(59, 4, 105),            "|".color(227, 173, 25),    "Publish current package"                       );
-    println!("   {}\t\t\t {} {}",   "help".color(59, 4, 105),               "|".color(227, 173, 25),    "Showes this help"                              );
-    println!("   {}\t {} {}",       "help (cmd/option)".color(59, 4, 105),  "|".color(227, 173, 25),    "Showes help for (cmd/option)"                  );
+    let arg_printer = Arg::new("A package manager for C++ by Cr0a3", "");
+    arg_printer.add_opt("-v, --version",        "Prints version info");
+    arg_printer.add_opt("--lib",                "Makes the new package a libary");
+    arg_printer.add_opt("--template (name)",    "Uses template (name)");
+    arg_printer.add_opt("build (target)",       "Builds current package with target (target)");
+    arg_printer.add_opt("clean",                "Cleans builds");
+    arg_printer.add_cmd("new (name)",           "Creates new package with name (name)");
+    arg_printer.add_opt("add",                  "Adds dependenci");
+    arg_printer.add_opt("publish",              "Publish current package");
+    arg_printer.add_opt("help",                 "Showes this help");
+    arg_printer.add_opt("help (cmd/option)",    "Showes help for (cmd/option)");
 }
 
 pub fn help_cmd(cmd: String) {
